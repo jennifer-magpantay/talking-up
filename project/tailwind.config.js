7ec835c3a52178f2ee84a7fe09507da8ecc60d93
Jennifer Magpantay <jennifer.magpantay@gmail.com>
2022-06-14 10:06:22 +0200

    chore: Set colour variables to Tailwind config

--- project/tailwind.config.js
+++ project/tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
+  mode: "jit",
   content: [
     "./src/**/*.tsx",
     "./src/components/**/*.tsx",
     "./src/screens/**/*.tsx",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#f59e0b",
+        accent: "#2f4858",
+        background: "#f3f4f6",
+        surface: "#f8fafc",
+      },
+    },
   },
   plugins: [],
 };
